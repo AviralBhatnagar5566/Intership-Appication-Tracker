5c35303b189e7f409af288c0c0770f10bd8fcfb1
AVIRAL BHATNAGAR <bhatnagaraviral1@gmail.com>
2026-06-17 14:03:16 +0530

    Handle frontend JS with fetch

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,4 @@
-
+const applicationForm = document.getElementById("applicationForm")
 
 const response = async function response(CompanyName,roleposition,date,status,applicationlink,notes) {
     try {
@@ -28,53 +28,60 @@ const response = async function response(CompanyName,roleposition,date,status,ap
         
     }
 }
-const CompanyName = document.getElementById("Companyname").value.trim()
-const roleposition = document.getElementById("roleposition").value.trim()
-const date = document.getElementById("date").value.trim()
-const status = document.getElementById("status").value.trim()
-const applicationlink = document.getElementById("applicationlink").value.trim()
-const notes = document.getElementById("notes").value.trim()
-const selectedDate = new Date(date)
-const today = new Date()
-today.setHours(0,0,0,0)
-let isvalue = true
+applicationForm.addEventListener("submit",async function (event) {
+     event.preventDefault();
+    const CompanyName = document.getElementById("CompanyName").value.trim()
+    const roleposition = document.getElementById("roleposition").value.trim()
+    const date = document.getElementById("date").value.trim()
+    const status = document.getElementById("status").value.trim()
+    const applicationlink = document.getElementById("applicationlink").value.trim()
+    const notes = document.getElementById("notes").value.trim()
+    const selectedDate = new Date(date)
+    const today = new Date()
+    today.setHours(0,0,0,0)
+    let isvalue = true
 
-document.getElementById("CompanyNameerror").innerText = "";
-document.getElementById("rolepositionerror").innerText = "";
-document.getElementById("dateerror").innerText = "";
-document.getElementById("sucessMsg").innerText = "";
-document.getElementById("statuserror").innerText = "";
-document.getElementById("applicationlinkerror").innerText = "";
-document.getElementById("notes").innerText = "";
+    document.getElementById("CompanyNameerror").innerText = "";
+    document.getElementById("rolepositionerror").innerText = "";
+    document.getElementById("dateerror").innerText = "";
+    document.getElementById("successMsg").innerText = "";
+    document.getElementById("statuserror").innerText = "";
+    document.getElementById("applicationlinkerror").innerText = "";
+    document.getElementById("noteserror").innerText = "";
 
 
-if(!CompanyName||!CompanyName.trim()){
-    document.getElementById("CompanyNameerror").innerText = "Write a companyname"
-    isvalue = false
+    if(!CompanyName||!CompanyName.trim()){
+        document.getElementById("CompanyNameerror").innerText = "Write a companyname"
+        isvalue = false
 }
-if(!roleposition||!roleposition.trim()){
-    document.getElementById("rolepositionerror").innerText = "Write a roleandposition"
-    isvalue = false
-}
-if(!date){
-    document.getElementById("dateerror").innerText = "fill the date"
-    isvalue = false
-}
-if(selectedDate < today){
-    document.getElementById("dateerror").innerText = "Date can't not be in past"
-}
-if(!status){
-    document.getElementById("statuserror").innerText = "Select the status"
-}
+    if(!roleposition||!roleposition.trim()){
+        document.getElementById("rolepositionerror").innerText = "Write a roleandposition"
+        isvalue = false
+    }
+    if(!date){
+        document.getElementById("dateerror").innerText = "fill the date"
+        isvalue = false
+    }
 
-if(isvalue){
-        const success = await response(CompanyName,roleposition,date,status,applicationlink,notes)
-        if(success){
-            document.getElementById("sucessMsg").innerText ="✅ Application submitted successfully!"
-            form.reset()
-        }
-       else{
-            document.getElementById("sucessMsg").innerText ="❌ SomeThing Went Wrong"
+    else if(selectedDate < today){
+        document.getElementById("dateerror").innerText = "Date can't not be in past"
+        isvalue = false
+    }
+    if(!status){
+        document.getElementById("statuserror").innerText = "Select the status"
+        isvalue = false
+    }
 
-       }     
-    }  
+    if(isvalue){
+            const success = await response(CompanyName,roleposition,date,status,applicationlink,notes)
+            if(success){
+                document.getElementById("successMsg").innerText ="✅ Application submitted successfully!"
+                applicationForm.reset()
+            }
+        else{
+                document.getElementById("successMsg").innerText ="❌ SomeThing Went Wrong"
+
+        }     
+        }  
+    
+})
