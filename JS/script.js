3803a1f1d4f64fcc5afb0624d963dbd7a7d2c1bd
AVIRAL BHATNAGAR <bhatnagaraviral1@gmail.com>
2026-06-17 13:49:31 +0530

    Handle frontend JS with fetch

--- JS/script.js
+++ JS/script.js
@@ -10,8 +10,8 @@ const response = async function response(CompanyName,roleposition,date,status,ap
                 roleposition,
                 date,
                 status,
-                applicationlink,
-                notes
+                applicationlink: applicationlink || "", 
+                notes: notes || ""
             }) 
         })
 
@@ -28,3 +28,53 @@ const response = async function response(CompanyName,roleposition,date,status,ap
         
     }
 }
+const CompanyName = document.getElementById("Companyname").value.trim()
+const roleposition = document.getElementById("roleposition").value.trim()
+const date = document.getElementById("date").value.trim()
+const status = document.getElementById("status").value.trim()
+const applicationlink = document.getElementById("applicationlink").value.trim()
+const notes = document.getElementById("notes").value.trim()
+const selectedDate = new Date(date)
+const today = new Date()
+today.setHours(0,0,0,0)
+let isvalue = true
+
+document.getElementById("CompanyNameerror").innerText = "";
+document.getElementById("rolepositionerror").innerText = "";
+document.getElementById("dateerror").innerText = "";
+document.getElementById("sucessMsg").innerText = "";
+document.getElementById("statuserror").innerText = "";
+document.getElementById("applicationlinkerror").innerText = "";
+document.getElementById("notes").innerText = "";
+
+
+if(!CompanyName||!CompanyName.trim()){
+    document.getElementById("CompanyNameerror").innerText = "Write a companyname"
+    isvalue = false
+}
+if(!roleposition||!roleposition.trim()){
+    document.getElementById("rolepositionerror").innerText = "Write a roleandposition"
+    isvalue = false
+}
+if(!date){
+    document.getElementById("dateerror").innerText = "fill the date"
+    isvalue = false
+}
+if(selectedDate < today){
+    document.getElementById("dateerror").innerText = "Date can't not be in past"
+}
+if(!status){
+    document.getElementById("statuserror").innerText = "Select the status"
+}
+
+if(isvalue){
+        const success = await response(CompanyName,roleposition,date,status,applicationlink,notes)
+        if(success){
+            document.getElementById("sucessMsg").innerText ="✅ Application submitted successfully!"
+            form.reset()
+        }
+       else{
+            document.getElementById("sucessMsg").innerText ="❌ SomeThing Went Wrong"
+
+       }     
+    }  
